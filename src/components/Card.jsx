0013c6c0e36938cdfe55151393fa503b99c1c968
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-28 22:01:57 +0530

    change API card to main

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,42 +1,9 @@
-import { useEffect, useState } from "react";
-
-export const Card = () => {
-  const [pokemon, setPokemon] = useState([]);
-  function handleCardClick() {
-    console.log("Card Click");
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
-        const data = await res.json();
-        console.log(data);
-
-        const pokeDetails = await Promise.all(
-          data.results.map(async (poke) => {
-            const res = await fetch(poke.url); // nested API call
-            const details = await res.json();
-            return {
-              id: details.id,
-              name: poke.name,
-              image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${details.id}.png`, // final image URL
-            };
-          })
-        );
-        setPokemon(pokeDetails);
-      } catch (err) {
-        console.error("Filled To Featch Data", err);
-      }
-    };
-    fetchData();
-  }, []);
-
+export const Card = ({ handleCardClick, pokemonData }) => {
   return (
     <>
-      {pokemon.map((poke) => (
+      {pokemonData.map((poke) => (
         <div
-          onClick={handleCardClick}
+          onClick={() => handleCardClick(poke.id)}
           key={poke.id}
           className="card border-2 flex flex-col items-center w-full py-5 px-3 rounded-xl hover:scale-105 transition-transform duration-300 hover:shadow-[0px_0px_41px_-5px_#1f1f23] active:scale-95"
         >
