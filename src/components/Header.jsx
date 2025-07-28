e06afde6d328daa4fe49bceae9b97cb3760280b6
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-28 22:02:15 +0530

    improev header score

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-export const Header = () => {
+export const Header = ({ score, highScore }) => {
   return (
     <header className="bg-gradient-to-t from-blue-700 to-indigo-600 flex h-20 items-center justify-between px-20 text-white">
       <h1 className=" text-4xl font-mono tracking-wider">Memory Card</h1>
       <div>
-        <p className="text-xl font-bold tracking-wide">Score: 0</p>
-        <p className="text-xl font-bold tracking-wide">Best Score: 0</p>
+        <p className="text-xl font-bold tracking-wide">Score: {score}</p>
+        <p className="text-xl font-bold tracking-wide">
+          Best Score: {highScore}
+        </p>
       </div>
     </header>
   );
